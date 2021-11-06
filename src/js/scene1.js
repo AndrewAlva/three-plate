aec386c9040690f814e1a7c30494071ab16e7d33
Andrew Alva <andrew@activetheory.net>
2021-11-06 11:34:26 -0600

    added 2 geometries to base scene

--- src/js/scene1.js
+++ src/js/scene1.js
@@ -12,17 +12,21 @@ const scene1 = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry2 = new THREE.BoxGeometry(5, 5, 5)
 const material = new THREE.ShaderMaterial({
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
     side: THREE.DoubleSide,
     transparent: true,
+    depthTest: false,
     uniforms: {
         uAlpha: { value: 0.8 },
     },
  })
 const mesh = new THREE.Mesh(geometry, material)
+const mesh2 = new THREE.Mesh(geometry2, material)
 scene1.add(mesh)
+scene1.add(mesh2)
 
 
 /**
